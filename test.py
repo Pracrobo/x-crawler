735a96f8b8afcd33c856c9bd0ad14e58b2cdc3e3
Pracrobo <cpu180621@gmail.com>
2025-07-07 17:47:23 +0900

    fix: dotenv 저장로 변경

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 from logger import get_logger
 from openpyxl import load_workbook
 from playwright.async_api import async_playwright, TimeoutError
@@ -9,13 +10,15 @@ from urllib.parse import urlparse
 import time 
 
 logger = get_logger()
+load_dotenv()
+CURRENT_TIME = datetime.now()
 
 def urlCollecter():
-    wb= load_workbook(FILE_PATH_3)
+    wb= load_workbook(os.getenv("TARGET_FILE_PATH"))
     ws = wb.active
 
     col_a, col_c = 'A', 'C'
-    urls = []
+    urls_group = []
     group = []
     empty_row_count = 0
     row = 3
@@ -25,7 +28,7 @@ def urlCollecter():
         c_value = ws[f'{col_c}{row}'].value
         if c_value is None:
             if group:
-                urls.append(group)
+                urls_group.append(group)
                 group = []
                 time.sleep(1)
             empty_row_count += 1
@@ -37,16 +40,17 @@ def urlCollecter():
             group.append(pair)
 
             if len(group) == 5:     
-                urls.append(group)
+                urls_group.append(group)
                 group = []
                 time.sleep(1)
         row += 1
     
     if group:
-        urls.append(group)
-    total_items = sum(len(g) for g in urls)
-    logger.info(f"총 {len(urls)}중 {total_items}개 url 모으기 끝")
-    return urls
+        urls_group.append(group)
+    total_items = sum(len(g) for g in urls_group)
+    logger.info(f"총 {len(urls_group)} 단위, {total_items}개 url 모으기 끝")
+
+    return urls_group
 
 
 async def extract_elements(content):
@@ -87,10 +91,12 @@ async def extract_elements(content):
 
     return result
 
-def failed_urls(url, error,  filename=FILE_PATH_2):
+def failed_urls(url, error,  filename=(os.getenv("FILE_PATH_2"))):
     """에러난 URL을 지정된 텍스트 파일에 한 줄씩 저장"""
+    file_dir = os.getenv("FAIL_PATH")
+    file_path = os.path.join(file_dir, f"{CURRENT_TIME.date()}-{filename}")
     try:
-        with open(filename, "a", encoding="utf-8") as f:
+        with open(file_path, "a", encoding="utf-8") as f:
             f.write(f"{url}: {error}\n")
     except Exception as e:
         logger.error(f"[URL 저장 실패] {url} → {e}")
@@ -103,9 +109,12 @@ def is_valid_url(url):
         return False
     
 
-def find_file(filename=FILE_PATH_2):
+def find_file(filename=(os.getenv("FILE_PATH_2"))):
+    """멈춘 파일이 있는지 확인"""
     urls = []
-    with open(filename, 'r', encoding='utf-8') as f:
+    file_dir=os.getenv("FAIL_PATH")
+    file_path = os.path.join(file_dir, f"{CURRENT_TIME.date()}-{filename}")
+    with open(file_path, 'r', encoding='utf-8') as f:
         for line in f:
             if ':' in line:
                 parts = line.strip().split(':', 2)  # 최대 2번만 split
@@ -118,12 +127,15 @@ def find_file(filename=FILE_PATH_2):
     chunked_urls = chunk_list(urls, chunk_size=5)
     return chunked_urls
 
+
 def chunk_list(lst, chunk_size=5):
     return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
     
 
 def to_excel(data_list):
-    file_dir = FILE_PATH_1
+    """결과 excel로 저장하기"""
+
+    file_dir = os.getenv("FILE_PATH_1")
     os.makedirs(file_dir, exist_ok=True)  # 폴더 없으면 자동 생성
 
     date_str = datetime.now().strftime("%Y-%m-%d")
@@ -134,6 +146,7 @@ def to_excel(data_list):
         views_values = (data.get("views_info") or []) + [None, None, None]
         views_values = views_values[:3]
         row = {
+        "No": data.get("no"),
         "멘션" : data.get("mention"),
         "게재 URL" : data.get("url"),
         "계정": data.get("id"),
@@ -147,22 +160,12 @@ def to_excel(data_list):
     df = pd.DataFrame(rows)
     
     try:
-        if os.path.exists(filename):
-            existing_df = pd.read_excel(filename)
-            start_no = len(existing_df) + 1
-        else:
-            start_no = 1
-        logger.info('df insert 시작')
-        
-        df.insert(0, "No", range(start_no, len(df) + start_no))
-        
         if not os.path.exists(filename):
             df.to_excel(filename, index=False)
         else:
             with pd.ExcelWriter(filename, engine='openpyxl', mode='a', if_sheet_exists='overlay') as w:
                 sheet = w.sheets["Sheet1"]
-                startrow = sheet.max_row
-                df.to_excel(w, index=False, header=False, startrow=startrow)
+                df.to_excel(w, index=False, header=False, startrow=sheet.max_row)
                 logger.info("행 단위 청크 엑셀 저장 완료")
 
     except Exception as e:
@@ -175,7 +178,7 @@ def to_excel(data_list):
 
 async def process_page(page, url):
     logger.debug(f"접속 시도: {url}")
-    await page.goto(url, timeout=30000)
+    await page.goto(url, timeout=30000) # Option DOM요소로딩 후 시작하게끔
     locator = page.locator("article")
     await locator.wait_for(state="visible", timeout=60000)
     content = await page.inner_text("article")
@@ -184,10 +187,11 @@ async def process_page(page, url):
     return result
 
 
-async def safe_process_page_with_retry(page, url, retries=3):
+async def retry_url(page, no, url, retries=3):
     for attempt in range(1, retries + 1):
         try:
             result = await process_page(page, url)
+            result["no"] = no
             return {"result": result, "url": url, "error": None}
         except Exception as e:
             logger.warning(f"{url} 처리 실패 {attempt}/{retries}: {e}")
@@ -202,22 +206,26 @@ async def main():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=True)
         context = await browser.new_context()
-        file_path  = FILE_PATH_2
-        if os.path.exists(file_path):
+        file_path  = os.getenv("FILE_PATH_2")
+        file_dir = os.getenv("FAIL_PATH")  # 경로 변경
+        os.makedirs(file_dir, exist_ok=True) 
+        filename = os.path.join(file_dir, f"{CURRENT_TIME.date()}-{file_path}")
+        if os.path.exists(filename):
             logger.info("실패한 파일 목록이 존재합니다.")
-            urls = find_file()
+            urls_group = find_file()
         else:
-            urls = urlCollecter() 
+            urls_group = urlCollecter() 
+            
         
-        for group in urls:
+        for group in urls_group:
             tasks = []
             pages = []
             logger.info('group 단위 페이지 탭 열기 시작')
 
-            for url in group:
+            for no, url in group:
                 page = await context.new_page()
                 pages.append(page)
-                tasks.append(safe_process_page_with_retry(page, url))
+                tasks.append(retry_url(page, no, url))
 
             results = await asyncio.gather(*tasks)
 
